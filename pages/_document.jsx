0dbbe61dfc268898bb3a3fb6ea0d8f422f6affbd
Zane Wilson <wilsonzanea@gmail.com>
2022-05-09 18:17:43 -0400

    page backgound color

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -15,7 +15,7 @@ export default function Document() {
                     rel="stylesheet"
                 />
             </Head>
-            <body>
+            <body className="bg-gray-300">
                 <Main />
                 <NextScript />
             </body>
